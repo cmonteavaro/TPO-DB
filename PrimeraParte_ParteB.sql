660e7d51c60c790519d7f6a9dab34d14335328fe
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-06-29 10:03:52 -0300

    Se agregaron PKs y FKs a PrimeraParte_Parte B

--- PrimeraParte_ParteB.sql
+++ PrimeraParte_ParteB.sql
@@ -4,7 +4,18 @@ create table Usuarios (
 	alias varchar(50),
 	contra varchar(100),
 	activo int,
-	correo varchar(200)
+	correo varchar(200),
+	constraint pk_Usuarios primary key (idUsuario)
+)
+go
+
+
+
+create table categorias(
+	idCategoria int,
+	nombre varchar(100),
+	descripcion varchar(200)
+	constraint pk_Categorias primary key (idCategoria)
 )
 go
 
@@ -15,29 +26,31 @@ create table noticias(
 	autor varchar(100),
 	fecha date,
 	ubicacion varchar(100)
-)
-go
-
-create table categorias(
-	idCategoria int,
-	nombre varchar(100),
-	descripcion varchar(200)
+	constraint pk_Noticias primary key (idNoticia),
+	constraint fk_Noticias_Categorias foreign key (idCategoria) references categorias,
 )
 go
 
 create table comentarios(
-	idComenatario int,
+	idComentario int,
 	idNoticia int,
 	texto varchar(200),
 	idUsuario int,
-	fecha date
+	fecha date,
+	constraint pk_Comentarios primary key (idComentario),
+	constraint fk_Comentarios_Noticias foreign key (idNoticia) references noticias,
+	constraint fk_Comentarios_Usuarios foreign key (idUsuario) references usuarios
 )
 go
+
+
 
 create table privilegios(
 	idUsuario int,
 	privilegio varchar(15),
 	idCategoria int
+	constraint fk_Privilegios_Usuarios foreign key (idUsuario) references usuarios,
+	constraint fk_Privilegios_Categorias foreign key (idCategoria) references categorias
 )
 go
 
@@ -53,5 +66,7 @@ create table votaciones(
 	votosUno int,
 	votosDos int,
 	votosTres int
+	constraint pk_Votaciones primary key (idVotacion),
+	constraint fk_Votaciones_Noticias foreign key (idNoticia) references noticias
 )
 go
